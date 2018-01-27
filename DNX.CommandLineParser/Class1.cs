b43d1cc10b31ccc4b3336035106bf30dcc3d92c2
Martin Smith <martinsmith1968@gmail.com>
2018-01-27 12:45:31 +0000

    Add project files.

---
+++ DNX.CommandLineParser/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNX.CommandLineParser
+{
+    public class Class1
+    {
+    }
+}
